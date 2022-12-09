0ae6e9084bc9891274afe49ad43cc0b65ce36e85
Circle <98963440+yohpx@users.noreply.github.com>
2022-12-10 00:29:25 +0700

    refactor extension to file

--- circleutils/bot.py
+++ circleutils/bot.py
@@ -23,12 +23,12 @@ class CircleUtils(commands.Bot):
             owner_id=389034898666553344
         )
 
-        for extension in os.listdir("./cogs"):
-            if extension.endswith(".py") and extension[:-3] not in IGNORED_MODULES:
+        for file in os.listdir("./cogs"):
+            if file.endswith(".py") and file[:-3] not in IGNORED_MODULES:
                 try:
-                    self.load_extension(f"cogs.{extension[:-3]}")
-                    print(f"[INFO] Loaded {extension[:-3]}.")
+                    self.load_extension(f"cogs.{file[:-3]}")
+                    print(f"[INFO] Loaded {file[:-3]}.")
                 except:
-                    print(f"[INFO] Failed to load {extension}", file=sys.stderr)
+                    print(f"[INFO] Failed to load {file}", file=sys.stderr)
                     traceback.print_exc()
         print(f"Ignored modules (disabled on start): {', '.join(IGNORED_MODULES)}")
