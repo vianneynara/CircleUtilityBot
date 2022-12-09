e444876c541c5c20d7abd0fad6f7974b903303d9
Circle <98963440+yohpx@users.noreply.github.com>
2022-12-09 22:48:54 +0700

    Fix typehint for is_owner

--- assets/tools/USER_CHECKERS.py
+++ assets/tools/USER_CHECKERS.py
@@ -3,5 +3,5 @@ from assets.CONFIG_CONSTANTS import OWNER_ID
 from typing import Any
 
 
-def is_owner(ctx: Any[disnake.Interaction | disnake.GuildCommandInteraction]):
-    return ctx.author.id == OWNER_ID
+def is_owner(context: Any) -> bool:
+    return context.author.id == OWNER_ID
