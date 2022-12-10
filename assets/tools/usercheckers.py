a17dc80581ebb02bfb4213b76278da9c615e4c27
Circle <98963440+yohpx@users.noreply.github.com>
2022-12-10 23:19:03 +0700

    Added moderator and admin permission checker

--- assets/tools/usercheckers.py
+++ assets/tools/usercheckers.py
@@ -1,6 +1,16 @@
+import disnake
+
 from assets.constants.ownerid import OWNER_ID
 from typing import Any
 
 
 def is_owner(context: Any) -> bool:
     return context.author.id == OWNER_ID
+
+
+def is_moderator(context: Any) -> bool:
+    return context.author.guild_permissions.moderate_members
+
+
+def is_admin(context: Any) -> bool:
+    return context.author.guild_permissions.administrator
