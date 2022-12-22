815a01de97469af89deac1b26ee9f2b893ab156c
Circle <98963440+yohpx@users.noreply.github.com>
2022-12-23 04:47:41 +0700

    Removed and merged exceptions to init file

--- assets/exceptions/__init__.py
+++ assets/exceptions/__init__.py
@@ -1,2 +1,38 @@
-from .invalid_target import InvalidTargetException
-from .invalid_duration import InvalidDurationException, DurationTooLongException
+class CommandDisabled(Exception):
+    """Thrown if a parameter is missing"""
+
+    def __init__(self, message="This command is disabled!"):
+        self.message = message
+        super(CommandDisabled, self).__init__(self.message)
+
+
+class InvalidDurationException(Exception):
+    """Thrown if the duration is invalid"""
+
+    def __init__(self, message="Duration is invalid!"):
+        self.message = message
+        super(InvalidDurationException, self).__init__(self.message)
+
+
+class DurationTooLongException(Exception):
+    """Thrown if the duration is too long"""
+
+    def __init__(self, message="Duration is too long!"):
+        self.message = message
+        super(DurationTooLongException, self).__init__(self.message)
+
+
+class InvalidTargetException(Exception):
+    """Thrown if the target is invalid"""
+
+    def __init__(self, message="Target is invalid!"):
+        self.message = message
+        super(InvalidTargetException, self).__init__(self.message)
+
+
+class RequiredParameterMissingException(Exception):
+    """Thrown if a parameter is missing"""
+
+    def __init__(self, message="A required parameter unfilled!"):
+        self.message = message
+        super(RequiredParameterMissingException, self).__init__(self.message)
--- assets/exceptions/invalid_target.py
+++
@@ -1,5 +0,0 @@
-class InvalidTargetException(Exception):
-    """Thrown if the target is invalid"""
-    def __init__(self, message="Target is invalid!"):
-        self.message = message
-        super(InvalidTargetException, self).__init__(self.message)
